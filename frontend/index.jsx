75fa8bc1ea6194fe1e234897d242bbe8c19d8fde
Jingna Li <jingna13@gmail.com>
2018-09-29 21:10:09 -0700

    Add index and root components

---
+++ frontend/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Root from './root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Document loaded");
+  ReactDOM.render(<Root />, root);
+});
---
+++ frontend/root.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ChampionIndex from './champion_index';
+
+const Root = () => (
+  <div>
+    <ChampionIndex />
+  </div>
+);
+
+export default Root;
