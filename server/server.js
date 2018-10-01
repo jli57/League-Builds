95505c71f92c78406ee70613232f9bae12178573
lljruffin <lljruffin@gmail.com>
2018-09-30 17:36:20 -1000

    Uninstall express-handlebars

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
 const expressValidator = require('express-validator');
 const session = require('express-session');
 const passport = require('passport');
