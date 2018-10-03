8399cff9603399c2712a236ce2e9163837f856c3
Jingna Li <jingna13@gmail.com>
2018-10-03 01:05:01 -0700

    Add explicit return to functions

--- frontend/utils/api_util.js
+++ frontend/utils/api_util.js
@@ -6,20 +6,20 @@ export const fetchChampions = () => {
   return $.getJSON(CHAMPION_DATA);
 }
 
-export const getChampionRotations = () => (
-    $.ajax({
+export const getChampionRotations = () => {
+  return $.ajax({
       type: "GET",
       url: `https://na1.api.riotgames.com/lol/platform/v3/champions/1?api_key=${API_KEY}`,
       dataType: "JSON",
       error: (xhr, status) => { console.log(xhr, status) },
     })
-);
+};
 
 export const fetchChampionIcon = (championName) => (
   `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${championName}.png`
 )
-//
-// export const getChampion = (championName, callback) => {
-//   $.getJSON(`http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion/${championName}.json`,
-//       callback )
-// }
+
+export const fetchChampionData = (championName, callback) => {
+  return $.getJSON(`http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion/${championName}.json`,
+      callback )
+}
