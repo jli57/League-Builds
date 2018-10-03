cab0ae3924e7afa5eb49bddd47267e161faa1bab
Jingna Li <jingna13@gmail.com>
2018-10-03 01:03:48 -0700

    Rename getChampion to fetchChampionData

--- frontend/actions/champion_actions.js
+++ frontend/actions/champion_actions.js
@@ -3,10 +3,6 @@ import * as APIUtil from '../utils/api_util';
 export const RECEIVE_CHAMPIONS = "RECEIVE_CHAMPIONS";
 export const FILTER_CHAMPIONS = "FILTER_CHAMPIONS";
 
-export const searchChampions = searchTerm => dispatch => {
-
-}
-
 export const receiveChampions = (champions) => ({
   type: RECEIVE_CHAMPIONS,
   champions
