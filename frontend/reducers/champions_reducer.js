9a7bf1600eefd2a73f9a8d570c87551214237187
Jingna Li <jingna13@gmail.com>
2018-10-03 01:00:18 -0700

    Add builds reducer

--- frontend/reducers/champions_reducer.js
+++ frontend/reducers/champions_reducer.js
@@ -1,11 +1,10 @@
-import { RECEIVE_CHAMPIONS} from '../actions/champion_actions';
+import { RECEIVE_CHAMPIONS } from '../actions/champion_actions';
 import merge from 'lodash/merge';
 
 const initialState = {};
 
 export const championsReducer = ( state = initialState, action ) => {
   Object.freeze(state);
-  console.log(action);
   switch(action.type) {
     case RECEIVE_CHAMPIONS:
       let newState = merge({}, state, action.champions);
--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,6 +1,12 @@
-export const allChampions = ({champions}) => {
-  console.log(champions);
+export const selectChampions = (champions) => {
   return Object.keys(champions).map(championName => champions[championName] );
+}
+
+export const selectChampion = (champion) => {
+  if ( $.isEmptyObject(champion) ) {
+    return []; 
+  }
+  return Object.values(champion)[0];;
 }
 
 export const filterChampions = (champions, searchTerm) => {
