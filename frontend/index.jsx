34ab44be47878d85cd1b3956dd42c329a21035f1
Jingna Li <jingna13@gmail.com>
2018-10-02 20:39:16 -0700

    Change root import path

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Root from './root';
+import Root from './components/root';
+import { configureStore } from './store/store.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log("Document loaded");
-  ReactDOM.render(<Root />, root);
+  const root = document.getElementById("root");
+  const store = configureStore();
+  ReactDOM.render(<Root store={store} />, root);
 });
