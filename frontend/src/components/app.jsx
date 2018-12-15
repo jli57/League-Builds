da1d86c89e325bdfd9e6bdc079008571b320bb49
Jingna Li <jingna13@gmail.com>
2018-12-14 16:36:01 -0800

    Add routes on app level

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ChampionSearchContainer from './champions/champions_search_container';
 import BuildContainer from './builds/build_container';
 import Header from './header/header';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div id="app">
       <Header />
       <section id="body">
-        <ChampionSearchContainer />
-        <BuildContainer />
+        <Route exact path="/" component={ ChampionSearchContainer} />
+        <Route exact path="/" component={ BuildContainer } />
       </section>
     </div>
   );
