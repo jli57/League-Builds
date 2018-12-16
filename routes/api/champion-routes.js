4f23a097877c13e33d627037a19ecc83151a6d44
lljruffin <lljruffin@gmail.com>
2018-12-15 16:03:14 -1000

    Delete unnecessary code

--- routes/api/champion-routes.js
+++ routes/api/champion-routes.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const RiotController = require('../controller/riot-controller');
 
-// latest patch versions can be found here https://ddragon.leagueoflegends.com/api/versions.json
-// api can be found here http://ddragon.leagueoflegends.com/cdn/8.24.1/data/en_US/champion.json
-
 router.get('/all', (req, res) => {
 	RiotController.getAllChampions(req, res);	
 });
@@ -21,14 +18,8 @@ router.get('/image/id/:id', (req, res) => {
 	RiotController.getChampionImageById(req, res);
 });
 
-router.get('/image/name/:name', (req, res) => {
-	console.log('in');
+router.get('/image/name/:name', (req, res) => {	
 	RiotController.getChampionImageByName(req, res);
 });
-
-router.get('/:id/stats', (req, res) => {
-	throw new Error('unimplemented');
-});
-
 
 module.exports = router;
