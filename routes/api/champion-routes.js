cd97afd11e756b1c63e406e9302580657b98a52f
lljruffin <lljruffin@gmail.com>
2018-12-15 21:16:06 -1000

    Correct and refactor champion api

--- routes/api/champion-routes.js
+++ routes/api/champion-routes.js
@@ -3,23 +3,21 @@ const router = express.Router();
 const ChampionController = require('../controller/champion-controller');
 
 router.get('/all', (req, res) => {
-	ChampionController.getAllChampions(req, res);	
+	ChampionController.getAllChampions(req, res);
 });
 
-router.get('/id/:id', (req, res) => {
-	ChampionController.getChampionById(req, res);
+router.get('/:noun', (req, res) => {
+	if (isNaN(req.params.noun))
+		ChampionController.getChampionByName(req, res);
+	else
+		ChampionController.getChampionById(req, res);
 });
 
-router.get('/name/:name', (req, res) => {
-	ChampionController.getChampionByName(req, res);
-});
-
-router.get('/image/id/:id', (req, res) => {
-	ChampionController.getChampionImageById(req, res);
-});
-
-router.get('/image/name/:name', (req, res) => {	
-	ChampionController.getChampionImageByName(req, res);
-});
+router.get('/:noun/image', (req, res) => {
+	if (isNaN(req.params.noun))
+		ChampionController.getChampionImageByName(req, res);
+	else
+		ChampionController.getChampionImageById(req, res);
+})
 
 module.exports = router;
--- routes/api/runes-reforged-routes.js
+++ routes/api/runes-reforged-routes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const RunesReforgedController = require('../controller/runes-reforged-controller');
 
-// http://ddragon.leagueoflegends.com/cdn/8.24.1/data/en_US/runesReforged.json
-
 router.get('/:id', (req, res) => {
 	RunesReforgedController.getRunesReforgedById(req, res);
 });
