9153bbdecca622a958418cf3d6a560bf88f98233
lljruffin <lljruffin@gmail.com>
2018-12-15 21:19:54 -1000

    Add get all items endpoint

--- routes/api/item-routes.js
+++ routes/api/item-routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const ItemController = require('../controller/item-controller');
 
+router.get('/all', (req, res) => {
+	ItemController.getAllItems(req, res);
+});
+
 router.get('/:id', (req, res) => {
 	ItemController.getItemById(req, res);
 })
