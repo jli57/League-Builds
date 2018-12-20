d5e68682476e76be9b7ce316b9f231e529619f85
lljruffin <lljruffin@gmail.com>
2018-12-20 00:07:45 -1000

    Complete should retrieve a champion by name

---
+++ backend/spec/champions/champion-spec.js
@@ -0,0 +1,49 @@
+describe("DDragon Champion", function () {
+	const axios = require('axios');
+	const config = {
+		baseURL : 'http://localhost:8000/ddragon/champions'
+	}
+
+	it("should retrieve all champions", async (done) => {
+		try {
+			let res = await axios.get('/all', config);
+			
+			expect(res.status).toBe(200);
+			expect(res.data).not.toEqual({});
+		} catch (ex) {
+			done.fail(ex.message);
+		}
+		done();
+	});
+
+	it("should retrieve a champion by name", async (done) => {
+		try {
+			let res = await axios.get('/Annie', config);
+
+			expect(res.status).toBe(200);
+			expect(res.data).not.toEqual({});
+		} catch (ex) {
+			done.fail(ex.message);
+		}
+		done();
+	});
+
+	it("should retrieve a champion image url by id", async (done) => {
+		try {
+			fail('unimplemented');
+		} catch (ex) {
+			done.fail(ex.message);
+		}
+		done();
+	});
+
+	it("should retrieve a champion image url by name", async (done) => {
+		try {
+			fail('unimplemented');
+		} catch (ex) {
+			done.fail(ex.message);
+		}
+		done();
+	});
+
+});
