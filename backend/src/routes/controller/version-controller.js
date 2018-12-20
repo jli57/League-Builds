055ef86ab0187782e124a8c1d22564d073a560e9
lljruffin <lljruffin@gmail.com>
2018-12-19 23:36:32 -1000

    Add version api

--- backend/src/routes/controller/version-controller.js
+++ backend/src/routes/controller/version-controller.js
@@ -4,25 +4,25 @@ const HttpError = require('../../errors/HttpError');
 
 const VersionController = function () {
 	let versionAPI = async () => {
-		return await axios.get(DDragonService.getPath(VERSION));
+		let versions = await axios.get(DDragonService.getPath(VERSION));
+
+		if (versions)
+			return versions;
+		else
+			throw new HttpError(404, 'Versions not found');
 	}
 
 	let getLatestVersion = async (req, res) => {
 		try {
-			let version = await versionAPI();
-
-			if (version)
-				res.status(200).json({ version: version.data[0] });
-			else
-				throw new HttpError(404, 'Version error');
+			res.status(200).json({ version: (await versionAPI()).data[0] });
 		} catch (ex) {
 			res.status(ex.statusCode || 500).json({ errors: ex.msg });
 		}
 	};
 
 	let getAllVersions = async (req, res) => {
-		try {
-			return await versionApi().data;
+		try {			
+			res.status(200).json({ versions: (await versionAPI()).data });
 		} catch (ex) {
 			res.status(ex.statusCode || 500).json({ errors: ex.msg });
 		}
@@ -30,11 +30,7 @@ const VersionController = function () {
 
 	let getVersion = async (req, res) => {
 		try {
-			let versions = await versionAPI();
-			if (versions)
-				res.status(200).json({ version: `${versions[req.params.key]}`});
-			else
-				throw new HttpError(404, 'Version error');
+			res.status(200).json({ version: `${(await versionAPI()).data[req.params.key]}` });
 		} catch (ex) {
 			res.status(ex.statusCode || 500).json({ errors: ex.msg });
 		}
