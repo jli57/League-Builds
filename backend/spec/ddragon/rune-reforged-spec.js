909ce3b9971480d49e9c78405d263a9d6b581336
lljruffin <lljruffin@gmail.com>
2018-12-20 23:52:43 -1000

    Complete test stubs

--- backend/spec/ddragon/rune-reforged-spec.js
+++ backend/spec/ddragon/rune-reforged-spec.js
@@ -6,7 +6,10 @@ describe('DDragon Rune Reforged', () => {
 
 	it('should retrieve all reforged runes', async (done) => {
 		try {
+			let res = await axios.get('/', config);
 
+			expect(res.status).toBe(200);
+			expect(res.data).toBeDefined();
 		} catch (ex) {
 			fail(ex.msg);
 		}
@@ -15,6 +18,14 @@ describe('DDragon Rune Reforged', () => {
 
 	it('should retrieve a reforged rune tree', async (done) => {
 		try {
+			let trees = ['8000', '8100', '8200', '8300', '8400'];
+			for (var i = 0; i < trees.length; i++) {
+				let res = await axios.get(`/tree/${trees[i]}`, config);
+
+				expect(res.status).toBe(200);
+				expect(res.data).toBeDefined();
+				expect(res.data.id).toEqual(tree[i]);
+			}
 
 		} catch (ex) {
 			fail(ex.msg);
@@ -24,7 +35,14 @@ describe('DDragon Rune Reforged', () => {
 
 	it('should retrieve a reforged rune by id', async (done) => {
 		try {
+			let trees = ['8100', '8214'];
+			for (var i = 0; i < trees.length; i++) {
+				let res = await axios.get(`/${trees[i]}`, config);
 
+				expect(res.status).toBe(200);
+				expect(res.data).toBeDefined();
+				expect(res.data.id).toEqual(tree[i]);
+			}
 		} catch (ex) {
 			fail(ex.msg);
 		}
@@ -33,10 +51,15 @@ describe('DDragon Rune Reforged', () => {
 
 	it('should retrieve all reforged rune image url by id', async (done) => {
 		try {
+			let id = '8230';
+			let res = await axios.get(`/${id}/image`, config);
 
+			expect(res.status).toBe(200);
+			expect(res.data).toBeDefined();
+			expect((await axios.get(res.data.src)).status).toBe(200);
 		} catch (ex) {
 			fail(ex.msg);
 		}
 		done();
-	})	
+	})
 });
