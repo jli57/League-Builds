d15444a6e52f14a841190c1f177fa4ca0dc0ff80
lljruffin <lljruffin@gmail.com>
2018-12-20 23:37:05 -1000

    Complete rune-reforged-spec.js stub

--- backend/spec/ddragon/rune-reforged-spec.js
+++ backend/spec/ddragon/rune-reforged-spec.js
@@ -4,4 +4,39 @@ describe('DDragon Rune Reforged', () => {
 		baseURL: 'http://localhost:8000/ddragon/runesreforged'
 	}
 
+	it('should retrieve all reforged runes', async (done) => {
+		try {
+
+		} catch (ex) {
+			fail(ex.msg);
+		}
+		done();
+	})
+
+	it('should retrieve a reforged rune tree', async (done) => {
+		try {
+
+		} catch (ex) {
+			fail(ex.msg);
+		}
+		done();
+	})
+
+	it('should retrieve a reforged rune by id', async (done) => {
+		try {
+
+		} catch (ex) {
+			fail(ex.msg);
+		}
+		done();
+	})
+
+	it('should retrieve all reforged rune image url by id', async (done) => {
+		try {
+
+		} catch (ex) {
+			fail(ex.msg);
+		}
+		done();
+	})	
 });
