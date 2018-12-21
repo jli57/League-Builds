4aaef41685aa872a24b0e76482dfcdd35279f9cb
lljruffin <lljruffin@gmail.com>
2018-12-20 21:37:53 -1000

    Add should get an item by id test

--- backend/spec/ddragon/item-spec.js
+++ backend/spec/ddragon/item-spec.js
@@ -7,10 +7,24 @@ describe('DDragon Item', function () {
 	it('should get all items', async (done) => {
 		try {
 			let res = await axios.get('/all', config);
-			
+
 			expect(res.status).toBe(200);
 			expect(res).toBeDefined();
 			expect(res.data['1001']).toBeDefined();
+		} catch (ex) {
+			fail(ex.msg);
+		}
+		done();
+	});
+
+	it('should get an item by id', async (done) => {
+		try {
+			let id = '1001'
+			let res = await axios.get(`/${id}`, config)
+			
+			expect(res.status).toBe(200);
+			expect(res.data.name).toBeDefined();
+
 		} catch (ex) {
 			fail(ex.msg);
 		}
