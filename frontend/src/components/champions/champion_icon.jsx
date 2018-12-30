c7706c8129708b333a023693aa477341845d4fbe
Jingna Li <jingna13@gmail.com>
2018-12-29 22:36:05 -0800

    Modify champion icon URL source

--- frontend/src/components/champions/champion_icon.jsx
+++ frontend/src/components/champions/champion_icon.jsx
@@ -1,23 +1,13 @@
 import 'babel-polyfill';
 import React from 'react';
-import { fetchChampionIcon } from '../../utils/api_util';
 
-let k = ''
-async function championIcon(name) {
-	try{
-		let response = await fetchChampionIcon(name);
+const ChampionIcon = ({champion}) => {
+  return (
+    <img className="champion-img-icon"
+      src={ champion.image.full }
+      alt={ champion.name }/>
+  );
+}
 
-		
-		return response.data.src;
-	}catch(ex){
-		console.log('ex');
-	}
-};
-
-const ChampionIcon = ({champion}) => (
-  <img className="champion-img-icon"
-    src={ Promise.resolve(championIcon(champion.id)) }
-    alt={ champion.name }/>
-)
 
 export default ChampionIcon;
--- frontend/src/utils/api_util.js
+++ frontend/src/utils/api_util.js
@@ -2,9 +2,6 @@ import 'babel-polyfill'
 import API_KEY from './api_key';
 import axios from 'axios';
 
-
-import axios from 'axios';
-
 export const CHAMPION_DATA = "http://ddragon.leagueoflegends.com/cdn/8.24.1/data/en_US/champion.json";
 
 // export const fetchChampions = () => {
@@ -16,11 +13,8 @@ export const CHAMPION_DATA = "http://ddragon.leagueoflegends.com/cdn/8.24.1/data
 
 
 export const fetchChampions = () => {
-  // return $.ajax({
-  //   type: "GET",
-  //   url: "/ddragon/champions/all"
-  // })
-	return $.getJSON(CHAMPION_DATA);
+  return axios.get('/api/ddragon/champions/all');
+	// return $.getJSON(CHAMPION_DATA);
 }
 
 export const getChampionRotations = () => {
@@ -32,36 +26,19 @@ export const getChampionRotations = () => {
 	})
 };
 
-export const fetchChampionIcon = (championName) => {
-	return axios.get(`/api/ddragon/champions/${championName}/image`);
 
-	// let k =
-	// 	.then(res => {
-	// 		//console.log(res.data.src)
-	// 		return res.data.src
-	// 	})
+export const fetchChampionData = (championName) => {
+  return $.getJSON(`http://ddragon.leagueoflegends.com/cdn/8.24.1/data/en_US/champion/${championName}.json`);
+	// try {
+	// 	axios.get(`http://localhost:8000/ddragon/champions/${championName}`)
+	// 		.then(res => console.log(res))
+	// 		.catch(ex => console.log(ex));
+	// 	return $.getJSON(`http://ddragon.leagueoflegends.com/cdn/8.24.1/data/en_US/champion/${championName}.json`,
+	// 		callback)
 
-	// //console.log(k);
-	// //try{
-	// //let k = await axios.get(`http://localhost:8000/api/ddragon/champions/${championName}/image`);
-	// //return `http://ddragon.leagueoflegends.com/cdn/8.24.1/img/champion/${championName}.png`;
-	// return k;
-
-	//req.then(success => )
-
-}
-
-export const fetchChampionData = (championName, callback) => {
-	try {
-		axios.get(`http://localhost:8000/ddragon/champions/${championName}`)
-			.then(res => console.log(res))
-			.catch(ex => console.log(ex));
-		return $.getJSON(`http://ddragon.leagueoflegends.com/cdn/8.24.1/data/en_US/champion/${championName}.json`,
-			callback)
-
-	} catch (ex) {
-		console.log(ex);
-	}
+	// } catch (ex) {
+	// 	console.log(ex);
+	// }
 }
 
 export const fetchChampionSkillIcon = (spellName) => (
