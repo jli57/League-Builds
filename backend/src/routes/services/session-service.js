1678363d3c891ebb756f35fc6038184a32fa9c9a
lljruffin <lljruffin@gmail.com>
2019-01-03 23:25:45 -1000

    Add getUserBuilds

--- backend/src/routes/services/session-service.js
+++ backend/src/routes/services/session-service.js
@@ -1,23 +1,33 @@
 const Session = require('../../models/session');
+const User = require('../../models/user');
 
 const SessionService = function () {
-    const getSession = async (id) => {
-        return await Session.findById(id);
-    };
+	const getSession = async (id) => {
+		return await Session.findById(id);
+	};
 
-    const deleteSession = async (session_id) => {
-        return await Session.findByIdAndDelete(session_id);
-    }
+	const deleteSession = async (session_id) => {
+		return await Session.findByIdAndDelete(session_id);
+	}
 
-    const createSession = async (userId) => {
-        return await Session.create(userId);
-    }
+	const createSession = async (userId) => {
+		return await Session.create(userId);
+	}
 
-    return {
-        getSession: getSession,
-        deleteSession: deleteSession,
-        createSession: createSession
-    }
+	const getUserBuilds = async (sessionId) => {
+		let session = await Session.findById(sessionId);
+		return await User.findById(session.userId, {
+			builds: 1,
+			username: 1,
+			_id: 0
+		});
+	};
+
+	return {
+		getSession: getSession,
+		deleteSession: deleteSession,
+		createSession: createSession
+	}
 }();
 
 module.exports = SessionService;
