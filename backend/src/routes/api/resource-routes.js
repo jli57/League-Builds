8ea3bc7c3d77f8e0421d170acfe01420b8db1b30
lljruffin <lljruffin@gmail.com>
2019-01-06 20:22:17 -1000

    Remove console.log

--- backend/src/routes/api/resource-routes.js
+++ backend/src/routes/api/resource-routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const ResourceController = require('../controller/resource-controller');
 
 router.put('/champion/:version', (req, res) => {
-	console.log('in');
 	ResourceController.insertChampions(req, res);
 });
 
