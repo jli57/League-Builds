1ca27225b9dfcf25bd7fa6641aed4d9362f09b4c
lljruffin <lljruffin@gmail.com>
2019-01-08 22:54:31 -1000

    Refactor and add upsertVersion stub

--- backend/src/routes/controller/resource-controller.js
+++ backend/src/routes/controller/resource-controller.js
@@ -2,32 +2,28 @@ const { DDragonService } = require('../services/ddragon-service');
 const HttpError = require('../../errors/HttpError');
 
 const ResourceController = function () {
-	let insertChampions = async (req, res) => {
-		try {
-			if (await DDragonService.insertChampions(req.params.version))
-				return res.status(201).json({});
-			else
-				throw new HttpError(406, 'Error');
-		} catch (ex) {
-			res.status(ex.statusCode || 500, ex.msg);
-		}
+   let insertChampions = async (req, res) => {
+      if (await DDragonService.insertChampions(req.params.version))
+         return res.status(201).json({});
+      else
+         throw new HttpError(406, 'Error');
    };
 
    let insertItems = async (req, res) => {
-		try {
-			if (await DDragonService.insertItems(req.params.version))
-				return res.status(201).json({});
-			else
-				throw new HttpError(406, 'Error');
-		} catch (ex) {
-			res.status(ex.statusCode || 500, ex.msg);
-		}
-	};
+      if (await DDragonService.insertItems(req.params.version))
+         return res.status(201).json({});
+      else
+         throw new HttpError(406, 'Error');
+   };
 
-	return {
+   let upsertVersion = async (req, res) => {
+
+   }
+
+   return {
       insertChampions: insertChampions,
       insertItems: insertItems
-	}
+   }
 }();
 
 module.exports = ResourceController;
