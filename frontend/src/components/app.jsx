31f9d1e6ceb0d13ab2c3908fc480e844f7cf61aa
Jingna Li <jingna13@gmail.com>
2019-01-10 19:27:00 -0800

    Add item frontend route

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChampionSearchContainer from './champions/champions_search_container';
 import BuildContainer from './builds/build_container';
+import ItemIndexContainer from './items/items_index_container';
 import Header from './header/header';
 import { Switch, Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ const App = () => {
       <section id="body">
         <Route exact exact path="/" component={ ChampionSearchContainer} />
         <Route exact path="/build" component={ BuildContainer } />
+        <Route exact path="/items" component={ ItemIndexContainer } />
       </section>
     </div>
   );
