8503faed1be86f80631096ddf3e91750a712e14b
lljruffin <lljruffin@gmail.com>
2019-01-13 22:03:47 -1000

    Add Resource to item-controller.js

--- backend/src/routes/controller/item-controller.js
+++ backend/src/routes/controller/item-controller.js
@@ -1,41 +1,49 @@
 const HttpError = require('../../errors/HttpError');
 const Item = require('../../models/item');
+const Resource = require('../../models/resource');
 
 const ItemController = function () {
-	let getAllItems = async (req, res) => {
-      const items =  await Item.find({}, {name: 1, "versions.8_24_1.image": 1 } ).sort({name: 1});
+   let latestVersion = async (req, res) => {
+      return (await Resource.findOne({})).versions[0].replace(/\./g, '_');
+   }
+
+   let getAllItems = async (req, res) => {
+      let latest = (await latestVersion())
+      const items = await Item.find({ [`versions.${latest}`]: { $exists: true } }, { name: 1, "versions.8_24_1.image": 1 }).sort({ name: 1 });
+
       let itemsData = {};
-      items.forEach( item => {
-        itemsData[item._id] = {
-          id: item._id,
-          name: item.name,
-          image: item.versions['8_24_1'].image
-        }
+      items.forEach(item => {
+         itemsData[item._id] = {
+            id: item._id,
+            name: item.name,
+            image: item.versions['8_24_1'].image
+         }
       });
       return res.status(200).json(itemsData);
-	};
+   };
 
-	let getItemById = async (req, res) => {
-      let item = await Item.findById(req.params.id);
-		  return res.status(200).json({
+   let getItemById = async (req, res) => {
+      let latest = (await latestVersion())
+      let item = await Item.findById({ _id: req.params.id, [`versions.${latest}`]: { $exists: true } });
+
+      return res.status(200).json({
          [item._id]: {
             id: item._id,
             name: item.name,
-            patch: '8.24.1',
-            tags: item.versions['8_24_1'].tags,
-            image: item.versions['8_24_1'].image,
-            description: item.versions['8_24_1'].description,
-            into: item.versions['8_24_1'].into,
-            gold: item.versions['8_24_1'].gold,
-            maps: item.versions['8_24_1'].maps
+            tags: item.versions[latest].tags,
+            image: item.versions[latest].image,
+            description: item.versions[latest].description,
+            into: item.versions[latest].into,
+            gold: item.versions[latest].gold,
+            maps: item.versions[latest].maps
          }
       });
-	};
+   };
 
-	return {
-		getAllItems: getAllItems,
-		getItemById: getItemById,
-	}
+   return {
+      getAllItems: getAllItems,
+      getItemById: getItemById,
+   }
 }();
 
 module.exports = ItemController;
